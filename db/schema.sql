c2a152a5a93d877565b8196c48f2458059b3d47a
kristafrancis721 <kristafrancis08@yahoo.com>
2022-08-28 20:15:28 -0400

    add votes table to schema

--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,22 @@
+DROP TABLE IF EXISTS votes;
 DROP TABLE IF EXISTS candidate;
 DROP TABLE IF EXISTS parties;
 DROP TABLE IF EXISTS voters;
+
+CREATE TABLE parties (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(50) NOT NULL,
+  description TEXT
+);
+
+CREATE TABLE candidate (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  party_id INTEGER,
+  industry_connected BOOLEAN NOT NULL,
+  CONSTRAINT fk_party FOREIGN KEY (party_id) REFERENCES parties(id) ON DELETE SET NULL
+);
 
 CREATE TABLE voters (
   id INTEGER AUTO_INCREMENT PRIMARY KEY,
@@ -10,19 +26,12 @@ CREATE TABLE voters (
   created_at DATETIME DEFAULT CURRENT_TIMESTAMP
 );
 
-
-CREATE TABLE parties (
+CREATE TABLE votes (
   id INTEGER AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(50) NOT NULL,
-  description TEXT
-);
-
-
-CREATE TABLE candidate (
-  id INTEGER AUTO_INCREMENT PRIMARY KEY,
-  first_name VARCHAR(30) NOT NULL,
-  last_name VARCHAR(30) NOT NULL,
-  party_id INTEGER,
-  industry_connected BOOLEAN NOT NULL,
-  CONSTRAINT fk_party FOREIGN KEY (party_id) REFERENCES parties(id) ON DELETE SET NULL
+  voter_id INTEGER NOT NULL,
+  candidate_id INTEGER NOT NULL,
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+  CONSTRAINT uc_voter UNIQUE (voter_id),
+  CONSTRAINT fk_voter FOREIGN KEY (voter_id) REFERENCES voters(id) ON DELETE CASCADE,
+  CONSTRAINT fk_candidate FOREIGN KEY (candidate_id) REFERENCES candidate(id) ON DELETE CASCADE
 );
